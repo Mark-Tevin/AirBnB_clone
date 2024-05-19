11aec614a68f4c92f2f5a37f3f783f30889a2f93
Mark-Tevin <tevinsmark@gmail.com>
2024-05-19 05:45:45 -0700

    basemodel correction

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,25 @@ from datetime import datetime
 
 class BaseModel:
     """Represents the Base Class of the Airbnb Clone class."""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        """Instatntiation of the BaseModel
+        Args:
+            *args (any): Not used
+            **kwargs (dict): key/value pairs to attributes
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """Updates updated_at with the current datetime"""
@@ -24,7 +38,25 @@ class BaseModel:
         rdict["updated_at"] = self.updated_at.isoformat()
         rdict["__class__"] = self.__class__.__name__
 
+        return rdict
+
     def __str__(self):
         """Returns the pribt/str representattion of BaseModel Instances"""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key, value in my_model_json.items():
+        print("\t{}: ({}) - {}".format(
+            key, type(value), value
+        ))
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -22,8 +22,22 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(my_model.updated_at)
 
     def test_save(self):
-        """Updates updated_at """
-        self.updated_at = datetime.utcnow()
+        my_model = BaseModel()
+
+        initial_updated_at = my_model.updated_at
+        current_updated_at = my_model.save()
+
+        self.assertNotEqual(initial_updated_at, current_updated_at)
+
+    def test_to_dict(self):
+        my_model = BaseModel()
+        rdict = my_model.to_dict()
+
+        self.assertIsInstance(rdict, dict)
+        self.assertEqual(rdict['__class__'], 'BaseModel')
+        self.assertEqual(rdict['id'], my_model.id)
+        self.assertEqual(rdict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(rdict['updated_at'], my_model.updated_at.isoformat())
 
     def test_str(self):
         my_model = BaseModel()
