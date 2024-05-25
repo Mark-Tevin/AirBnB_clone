7f226a3d6de1793b79328f623c1a744c4dbff02a
Mark-Tevin <tevinsmark@gmail.com>
2024-05-25 05:05:07 -0700

    codereview

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,56 @@
 #!/usr/bin/python3
-"""Defines all common attributes/methods fro other classes."""
+"""This script is the base model"""
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Represents the Base Class of the Airbnb Clone class."""
+
+    """Class from which all other classes will inherit"""
+
     def __init__(self, *args, **kwargs):
-        """Instatntiation of the BaseModel
+        """Initializes instance attributes
+
         Args:
-            *args (any): Not used
-            **kwargs (dict): key/value pairs to attributes
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
         """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-
-    def save(self):
-        """Updates updated_at with the current datetime"""
-        self.updated_at = datetime.utcnow()
-
-    def to_dict(self):
-        """Returns a dictionary containing all key/values of __dict__"""
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-
-        return rdict
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """Returns the pribt/str representattion of BaseModel Instances"""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """Returns string representation"""
 
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key, value in my_model_json.items():
-        print("\t{}: ({}) - {}".format(
-            key, type(value), value
-        ))
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        rdict = self.__dict__.copy()
+        rdict["__class__"] = type(self).__name__
+        rdict["created_at"] = my_dict["created_at"].isoformat()
+        rdict["updated_at"] = my_dict["updated_at"].isoformat()
+        return rdict
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,33 +1,94 @@
 #!/usr/bin/python3
-"""Defines unit tests for models/base_model.py
+"""Unittest module for the BaseModel Class."""
 
-Unittest classes:
-    Test_init
-    Test_save
-    Test_to_dict
-    Test_str
-"""
-
-import unittest
-from datetime import datetime
+from models import storage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+import json
+import os
+import re
+import time
+import unittest
+import uuid
 
 
 class TestBaseModel(unittest.TestCase):
-    def test_init(self):
-        my_model = BaseModel()
 
+    """Test cases for the BaseModel Class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods"""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_init(self):
+        """Tests instantiation of BaseModel class."""
+
+        my_model = BaseModel()
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
         self.assertIsNotNone(my_model.updated_at)
+        self.assertIsInstance(my_model, BaseModel)
+
+    def test_init_no_args(self):
+        """Tests __init__ with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.__init__()
+        msg = "__init__() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_init_many_args(self):
+        """Tests __init__ with many arguments."""
+        self.resetStorage()
+        args = [i for i in range(1000)]
+        my_model = BaseModel(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        my_model = BaseModel(*args)
+
+    def test_attributes(self):
+        """Tests attribute value for instance of a BaseModel class."""
+
+        attributes = storage.attributes()["BaseModel"]
+        o = BaseModel()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+    def test_datetime_created(self):
+        """Tests if updated and created date and time are current."""
+        date_now = datetime.now()
+        my_model = BaseModel()
+        diff = my_model.updated_at - my_model.created_at
+        self.assertTrue(abs(diff.total_seconds()) < 0.01)
+        diff = my_model.created_at - date_now
+        self.asserTrue(abs(diff.total_seconds()) < 0.1)
+
+    def test_user_id(self):
+        """Tests for unique user ids."""
+
+        nl = [BaseModel().id for i in range(1000)]
+        self.assertEqual(len(set(nl)), len(nl))
 
     def test_save(self):
+        """Tests the public instance methods save()."""
+
         my_model = BaseModel()
-
-        initial_updated_at = my_model.updated_at
-        current_updated_at = my_model.save()
-
-        self.assertNotEqual(initial_updated_at, current_updated_at)
+        time.sleep(0.5)
+        date_now = datetime.now()
+        my_model.save()
+        diff = my_model.updated_at - date_now
+        self.assertTrue(abs(diff.total_seconds()) < 0.01)
 
     def test_to_dict(self):
         my_model = BaseModel()
