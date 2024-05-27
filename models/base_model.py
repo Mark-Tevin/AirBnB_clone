5b840b57ab5286d38bd039240d61b2a3ab574a1e
Mark-Tevin <tevinsmark@gmail.com>
2024-05-27 07:43:37 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,28 +3,40 @@
 BaseModel module
 """
 
-import uuid
-from datetime import datetime, timezone
+from datetime import datetime
+from uuid import uuid4
 
 
 class BaseModel:
     """
     A base class for other models
     """
+    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel
+        Args:
+            - *args: Not used here
+            - **kwargs: dictionary of key-values arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now(timezone.utc)
-        self.updated_at = datetime.now(timezone.utc)
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k in ['created_at', 'updated_at'] and isinstance(v, str):
+                    v = datetime.strptime(v, self.TIME_FORMAT)
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self) -> None:
         """
         Updates the updated_at attribute with current time
         """
-        self.updated_at = datetime.now(timezone.utc)
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self) -> dict:
         """
@@ -42,18 +54,3 @@ class BaseModel:
         """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(
-            key, type(my_model_json[key]), my_model_json[key])
-        )
