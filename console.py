5cbb84a6f9c2c7b055e297b7a130abcb06658981
Mark-Tevin <tevinsmark@gmail.com>
2024-06-04 20:03:13 -0700

    updated Show

--- console.py
+++ console.py
@@ -23,7 +23,7 @@ class HBNBCommand(cmd.Cmd):
         cmd (cmd.Cmd): The base class for building command line interfaces.
     """
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
     valid_classes = [
         "BaseModel", "User", "Amenity",
         "Place", "Review", "State", "City"
@@ -142,6 +142,8 @@ class HBNBCommand(cmd.Cmd):
         command = arg_list[1].split('(')
         incoming_method = command[0]
 
+        incoming_xtra_arg = command[1].split(')')[0]
+
         method_dict = {
             'all': self.do_all,
             'show': self.do_show,
@@ -151,7 +153,8 @@ class HBNBCommand(cmd.Cmd):
         }
 
         if incoming_method in method_dict.keys():
-            return method_dict[incoming_method]("{} {}".format(incoming_class, ''))
+            return method_dict[incoming_method](
+                "{} {}".format(incoming_class, incoming_xtra_arg))
 
         print("*** Unknown syntax: {}".format(arg))
         return False
